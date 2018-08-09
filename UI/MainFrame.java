188f3455fca55be636f3cb2c7520a747bb04fbce
julis <617669559@qq.com>
2018-08-09 15:30:12 +0800

    第一次

---
+++ UI/MainFrame.java
@@ -0,0 +1,18 @@
+package DCL.UI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Julis on 17/8/5.
+ * User:Julis 落叶挽歌
+ * Date:17/8/5
+ * Time:下午2:57
+ */
+public class MainFrame extends JFrame {
+   public static void main(String []args){
+        new Frame();
+   }
+}
